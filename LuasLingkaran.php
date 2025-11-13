adc49a3e0ebf617303193ed6ff7af5a5c88ade84
ronal pardede IF24D <pardederonal58@gmail.com>
2025-11-13 16:26:47 +0700

    LuasLingkaran.php

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -1,14 +1,13 @@
-<?php
 
-namespace App\Math;
+<?php
 
 class LuasLingkaran {
 
     public const phi = 3.14;
-    protected int $jari;
+    public int $jari;
     
     public function __construct($isiJari = 1) {
-        $this->jari = $isiJari; //10
+        $this->jari = $isiJari;
     }
 
     public function tampil($nama = 'ban') {
@@ -27,3 +26,8 @@ class LuasLingkaran {
     }
 }
 
+$lingkaran = new LuasLingkaran(12);
+// $lingkaran->jari = 12;
+$lingkaran->tampil('roda'); //panggil method tampil
+
+LuasLingkaran::testing(); //panggil static method
